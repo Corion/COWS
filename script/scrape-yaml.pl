68c680b4c4d069ac5c73154c0c78d077bdf96185
Max Maischein <corion@corion.net>
2023-05-14 15:52:29 +0200

    Add RSS output

--- script/scrape-yaml.pl
+++ script/scrape-yaml.pl
@@ -1,7 +1,5 @@
 #!perl
 use 5.020;
-use Mojolicious;
-
 
 package Scraper::FromYaml 0.1;
 use 5.020;
@@ -51,6 +49,42 @@ sub fetch_data( $self, $id) {
     return $res->body
 }
 
+sub parse( $self, $rules, $id_or_html ) {
+    my $html = $id_or_html;
+    if( $id_or_html !~ /^</ ) {
+        $html = $self->fetch_data( $id_or_html );
+    };
+    return scrape( $html, $rules, { debug => 1, mungers => $self->mungers });
+}
+
+package main;
+
+use Getopt::Long;
+
+# Read merchant whitelist from config?
+use YAML 'LoadFile';
+use Filesys::Notify::Simple;
+use File::Spec;
+use JSON;
+use XML::Feed;
+use DateTime;
+use DateTime::Format::ISO8601;
+
+GetOptions(
+    'config|c=s'      => \my $config_file,
+    'interactive|i'   => \my $interactive,
+    'output-type|o=s' => \my $output_type,
+    'debug|d'         => \my $debug,
+);
+
+sub load_config( $config_file ) {
+    my $config = LoadFile( $config_file );
+    if( ! $config->{items} ) {
+        die "$config_file: No 'items' section found";
+    }
+    return $config;
+}
+
 sub extract_price( $text ) {
     $text =~ s/.*?(\d+)[.,](\d\d).*/$1.$2/r
 }
@@ -62,35 +96,6 @@ sub compress_whitespace( $text ) {
     return $text
 }
 
-
-sub parse( $self, $rules, $id_or_html ) {
-    my $html = $id_or_html;
-    if( $id_or_html !~ /^</ ) {
-        $html = $self->fetch_data( $id_or_html );
-    };
-    return scrape( $html, $rules, { debug => 1, mungers => $self->mungers });
-}
-
-use Getopt::Long;
-
-# Read merchant whitelist from config?
-use YAML 'LoadFile';
-use Filesys::Notify::Simple;
-use File::Spec;
-
-GetOptions(
-    'config|c=s'    => \my $config_file,
-    'interactive|i' => \my $interactive,
-);
-
-sub load_config( $config_file ) {
-    my $config = LoadFile( $config_file );
-    if( ! $config->{items} ) {
-        die "$config_file: No 'items' section found";
-    }
-    return $config;
-}
-
 my %handlers = (
     extract_price => \&extract_price,
     compress_whitespace => \&compress_whitespace,
@@ -100,6 +105,7 @@ sub create_scraper( $config ) {
     my $scraper = Scraper::FromYaml->new(
         mungers => \%handlers,
         base => $config->{base},
+        debug => $debug,
     );
 }
 
@@ -107,35 +113,74 @@ my %cache;
 sub scrape_pages($config, @items) {
     my $scraper = create_scraper( $config );
 
+    my $start_rule = 'items';
+    if( $output_type eq 'rss' ) {
+        $start_rule = 'rss';
+    }
+
     my @rows;
     for my $item (@items) {
-
         my $html = $cache{ $item } // $scraper->fetch_data( $item );
-        my $data = $scraper->parse($config->{items}, $html);
-        push @rows, map { $_->{item} = $item; $_ } @{$data};
-    }
-
-    my @columns;
-    if( ! $config->{columns}) {
-        my %columns;
-        for(@rows) {
-            for my $k (keys %$_) {
-                $columns{ $k } = 1;
-            }
+        my $data = $scraper->parse($config->{$start_rule}, $html);
+        if( ref $data eq 'HASH' ) {
+            push @rows, $data;
+        } else {
+            push @rows, @{$data};
         }
-        @columns = sort keys %columns;
-    } else {
-        @columns = $config->{columns}->@*;
     }
 
-    # Should we roll-up/flatten anything if the output format is text/table
-    # instead of JSON ?!
+    if( $output_type eq 'table' ) {
 
-    # Can we usefully output things as RSS ?!
-    use Text::Table;
-    my $res = Text::Table->new(@columns);
-    $res->load(map { [@{ $_ }{ @columns }] } @rows);
-    say $res;
+        my @columns;
+        if( ! $config->{columns}) {
+            my %columns;
+            for(@rows) {
+                for my $k (keys %$_) {
+                    $columns{ $k } = 1;
+                }
+            }
+            @columns = sort keys %columns;
+        } else {
+            @columns = $config->{columns}->@*;
+        }
+
+        # Should we roll-up/flatten anything if the output format is text/table
+        # instead of JSON ?!
+
+        # Can we usefully output things as RSS ?!
+        use Text::Table;
+        my $res = Text::Table->new(@columns);
+        $res->load(map { [@{ $_ }{ @columns }] } @rows);
+        say $res;
+    } elsif( $output_type eq 'rss' ) {
+        # XML::Feed
+        my $f = $rows[0];
+
+        my $feed = XML::Feed->new( 'Atom', version => 2 );
+
+        $feed->id("http://".time.rand()."/");
+        $feed->title($f->{title});
+        #$feed->link(...); # self-url!
+        #$feed->self_link($cgi->url( -query => 1, -full => 1, -rewrite => 1) );
+        #$feed->modified(DateTime->now);
+
+        for my $post (@{ $f->{entries} }) {
+            my $entry = XML::Feed::Entry->new();
+            $entry->id($post->{id});
+            $entry->link($post->{link});
+            $entry->title($post->{title});
+            $entry->summary($post->{summary} // "");
+            $entry->content($post->{content} );
+
+            my $p = DateTime::Format::ISO8601->new;
+            $entry->modified($p->parse_datetime( $post->{modified} ));
+
+            $entry->author($post->{author});
+            $feed->add_entry($entry);
+        }
+
+        say $feed->as_xml;
+    }
 }
 
 sub do_scrape_items ( @items ) {
