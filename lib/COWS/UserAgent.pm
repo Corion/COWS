13b6b3342a018069351c1e4adf6a30a739ad7b31
Max Maischein <corion@corion.net>
2023-05-18 20:47:21 +0200

    Add documentation

--- lib/COWS/UserAgent.pm
+++ lib/COWS/UserAgent.pm
@@ -53,6 +53,12 @@ COWS::UserAgent - useragent for scraping
       },
   }, $html, { url => $url });
 
+=head1 ACCESSORS
+
+=head2 C<< ->ua >>
+
+The C<Mojo::UserAgent> to be used for fetching pages.
+
 =cut
 
 has 'ua' => (
@@ -64,17 +70,51 @@ has 'ua' => (
     }
 );
 
+=head2 C<< ->base >>
+
+The URL to be fetched from.
+
+If the URL contains C<< %s >>, this will
+be replaced by the item passed to C<< ->fetch_item >>.
+
+=cut
+
 has 'base' => (
     is => 'ro',
 );
+
+=head2 C<< ->config >>
+
+The config as passed in.
+
+=cut
 
 has 'config' => (
     is => 'ro',
 );
 
+=head2 C<< ->mungers >>
+
+A hashref of subroutine references that are used to post-process the items
+as specified in the configuration.
+
+The subroutine gets passed three parameters:
+
+  sub munger($text, $node, $info) {
+    ...
+  }
+
+The C<$info> hash is passed through from the call to C<< ->parse >>.
+
+=cut
+
 has 'mungers' => (
     is => 'ro',
 );
+
+=head1 METHODS
+
+=cut
 
 sub make_url( $self, $id ) {
     my $base = $self->base;
@@ -94,6 +134,30 @@ sub fetch_item( $self, $id) {
     return $self->fetch( $self->make_url($id));
 }
 
+=head2 C<< ->parser >>
+
+  my $data = $ua->parse({
+      items => {
+          query => "article.message",
+          anonymous => 1,
+
+          columns => [
+            { name =>  'author',
+              query =>  "./@data-author",
+              single =>  1,
+            },
+            { name =>  modified,
+              query =>  'header ul.message-attribution-main time@datetime',
+              single =>  1,
+            },
+          ]
+      }
+  }, 'searchitem', {
+      url => $url, # info passed to
+  });
+
+=cut
+
 sub parse( $self, $rules, $id_or_html, $options ) {
     my $html = $id_or_html;
     if( $id_or_html !~ /^</ ) {
@@ -107,3 +171,9 @@ sub parse( $self, $rules, $id_or_html, $options ) {
 }
 
 1;
+
+=head1 CONFIGURATION
+
+The configuration consists of several parts
+
+=cut
